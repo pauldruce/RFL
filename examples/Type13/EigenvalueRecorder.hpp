32879528fcd23440ce80f1ca6bbe679ce532bb1f
Paul Druce <pjdruce@gmail.com>
2023-08-30 22:35:24 +0100

    Add documentation to EigenvalueRecorder class
    Added detailed documentation to EigenvalueRecorder class to enhance code readability and understanding. The comments provide a clear understanding of the purpose and functionality of EigenvalueRecorder class and its recordEigenvalues method. These improvements aim to ease the process of code maintenance and modification.

--- examples/Type13/EigenvalueRecorder.hpp
+++ examples/Type13/EigenvalueRecorder.hpp
@@ -12,12 +12,29 @@
 #include <iomanip>
 #include <utility>
 
+/**
+ * @class EigenvalueRecorder
+ * @brief A simple class to record the eigenvalues of a DiracOperator to a file.
+ *
+ * This recorder uses a HDF5 file to store the eigenvalues. The file name/location is
+ * set by the parameters of the simulation - specifically the type of the DiracOperator and
+ * the g2 value of action.
+ * Each recorder writes to a dataset group in the HDF5 file named by the constructor
+ * parameter "simulationId". Each eigenvalue data set is appended to this dataset group.
+ *
+ */
 class EigenvalueRecorder {
 public:
   EigenvalueRecorder(const DiracOperator& dirac, double g2, std::string simulationId)
       : m_dirac(dirac), m_g2(g2), m_simulationId(std::move(simulationId)) {
   }
 
+  /**
+   * @brief Records the eigenvalues of a given Dirac operator.
+   *
+   * The recordEigenvalues function accepts a diracId as input and records the eigenvalues of
+   * the dirac operator to a file.
+   */
   void recordEigenvalues(int diracId);
 
 private:
