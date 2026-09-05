6e68a2d8fb9e9b0f68e60730279ed3806c8fe40d
Paul Druce <pjdruce@gmail.com>
2026-09-05 09:21:16 +0100

    test(ci): verify pipeline triggers on C++ source modification

--- src/RFL/core/Simulation.cpp
+++ src/RFL/core/Simulation.cpp
@@ -4,5 +4,6 @@
 
 #include "Simulation.hpp"
 
+// Initialise the simulation with a Dirac operator and a Monte Carlo algorithm.
 Simulation::Simulation(std::unique_ptr<DiracOperator>&& dirac, std::unique_ptr<IAlgorithm>&& monte_carlo_algorithm)
     : m_dirac(std::move(dirac)), m_algorithm(std::move(monte_carlo_algorithm)){};
